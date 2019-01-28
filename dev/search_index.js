68b10d5eef421751c3401789f8514f0819ff064c
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-01-28 20:31:08 +0000

    build based on 5525489

---
+++ dev/search_index.js
@@ -0,0 +1,51 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#StateSpaceModels.jl-Documentation-1",
+    "page": "Home",
+    "title": "StateSpaceModels.jl Documentation",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "#Overview-1",
+    "page": "Home",
+    "title": "Overview",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "#Installation-1",
+    "page": "Home",
+    "title": "Installation",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "manual/#",
+    "page": "Manual",
+    "title": "Manual",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "manual/#Manual-1",
+    "page": "Manual",
+    "title": "Manual",
+    "category": "section",
+    "text": ""
+},
+
+]}
