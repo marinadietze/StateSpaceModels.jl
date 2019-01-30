4dc44d010330eab7f5d72d5da923e2d475ad415f
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-01-30 16:39:56 +0000

    build based on 7780176

--- dev/search_index.js
+++ dev/search_index.js
@@ -17,19 +17,27 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "#Overview-1",
-    "page": "Home",
-    "title": "Overview",
-    "category": "section",
-    "text": ""
-},
-
-{
     "location": "#Installation-1",
     "page": "Home",
     "title": "Installation",
     "category": "section",
-    "text": ""
+    "text": "This package is unregistered so you will need to Pkg.add it as follows:Pkg.add(\"https://github.com/LAMPSPUC/StateSpaceModels.jl.git\")"
+},
+
+{
+    "location": "#Notes-1",
+    "page": "Home",
+    "title": "Notes",
+    "category": "section",
+    "text": "This package is under development and some features may be changed or added.StateSpaceModels.jl is a package for modeling, forecasting and simulating time series in a state space framework.Estimation is done through function statespace and will automatically be run in parallel with all the currently active threads.Simulation of scenarios is done through function simulate."
+},
+
+{
+    "location": "#Features-1",
+    "page": "Home",
+    "title": "Features",
+    "category": "section",
+    "text": "Current features:Basic structural model (level, slope, seasonal)\nExogenous variables\nSquare-root Kalman Filter and smoother\nBig Kappa initialization\nMonte Carlo simulation\nParallel MLE estimation\nMultivariate modelingFuture features (work in progress):User-defined model\nCycles\nForecasting and confidence intervals\nCompletion of missing values\nDifferent models for each variable in multivariate case\nStructural break\nExact initialization"
 },
 
 {
